8575d31910bb6d5a490a224b15f3d1c7464c5668
Diaz <diaz_zeng@outlook.com>
2020-09-30 00:10:27 +0800

    增加轨迹球控制器

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date: 2020-09-28 20:48:01
  * @LastEditors: 曾令宇
- * @LastEditTime: 2020-09-29 00:35:28
+ * @LastEditTime: 2020-09-30 00:06:51
  * @FilePath: \gx-demo\src\views\About.vue
 -->
 <template>
@@ -10,7 +10,8 @@
 <script>
 import * as Three from "three";
 import * as D3 from "d3-geo";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
 export default {
   name: "about",
   data() {
@@ -26,7 +27,7 @@ export default {
       const loader = new Three.FileLoader();
       const promise = new Promise((res, rej) => {
         loader.load(
-          "/geo/geo_data.json",
+          "/geo/guangxi.json",
           (data) => {
             res(JSON.parse(data));
           },
@@ -40,15 +41,16 @@ export default {
     },
     initMap(geoJson) {
       const map = new Three.Object3D();
+      const [cx, cy] = [108.320004, 22.82402];
       const projection = D3.geoMercator()
-        .center([108.320004, 22.82402])
+        .center([cx, cy])
         .scale(80)
         .translate([0, 0]);
       geoJson.features.forEach((el) => {
         const city = new Three.Object3D();
         const coordinates = el.geometry.coordinates;
         const extrudeSettings = {
-          depth: 2.9,
+          depth: 1,
           bevelEnabled: false,
         };
         coordinates.forEach((multiPolygon) => {
@@ -58,12 +60,13 @@ export default {
               color: "white",
             });
             const lineGeometry = new Three.Geometry();
-            polygon.forEach(([x, y], index) => {
+            polygon.forEach((grid, index) => {
+              const [x, y] = projection(grid);
               if (index === 0) {
                 shape.moveTo(x, -y);
               }
               shape.lineTo(x, -y);
-              lineGeometry.vertices.push(new Three.Vector3(x, -y, 3));
+              lineGeometry.vertices.push(new Three.Vector3(x, -y, 1));
             });
             const geometry = new Three.ExtrudeGeometry(shape, extrudeSettings);
             const material = new Three.MeshBasicMaterial({
@@ -89,21 +92,50 @@ export default {
         }
         map.add(city);
       });
+      // this.camera.position.set(map.position.x, map.position.y, 50);
+      // this.camera.lookAt(map.position);
+      const [xx, yy] = projection([cx, cy]);
+      console.log([xx, yy]);
+      this.camera.position.set(xx, -yy, 30);
+      this.camera.lookAt(map.position);
+      // this.camera.rotation.set(-90, 0, 0);
+      // new Three.Camera().lookAt();
+      // new Three.Camera().position.set()
+      // this.camera.position.set(0, -70, 90);
+      // this.camera.lookAt(0, 0, 0);
       return map;
     },
     setLight() {
       let ambientLight = new Three.AmbientLight(0xffffff); // 环境光
       this.scene.add(ambientLight);
+      this.controls.update();
     },
     setControls() {
-      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+      this.controls = new TrackballControls(
+        this.camera,
+        this.renderer.domElement
+      );
+      this.controls.enablePan = false;
+    },
+    // setViewer(x,y,z){
+    //   this.camera.lookAt()
+    // },
+    render() {
+      this.renderer.render(this.scene, this.camera);
+    },
+    animate() {
+      requestAnimationFrame(this.animate);
+      this.controls.update();
+      this.render();
+      // console.log(this.camera);
+      this.controls.update();
     },
   },
   mounted() {
     this.renderer = new Three.WebGLRenderer();
     // this.renderer.setClearColor(0xb9d3ff, 1);
     this.renderer.setSize(this.clientWidth, this.clientHeight);
-    this.containerEl.appendChild(this.renderer.domElement);
+    this.containerEl.append(this.renderer.domElement);
     // document.body.appendChild(this.renderer.domElement);
     this.scene = new Three.Scene();
     this.camera = new Three.PerspectiveCamera(
@@ -112,13 +144,13 @@ export default {
       0.1,
       1000
     );
-    this.camera.position.set(0, -70, 150);
-    this.camera.lookAt(0, 0, 0);
+    // this.camera.position.set(this.clientWidth / 2, this.clientHeight / 2, 150);
+    // this.camera.lookAt(0, 0, 0);
     this.setControls();
     // this.setLight();
     this.loadGeoData().then((data) => {
       this.scene.add(this.initMap(data));
-      this.renderer.render(this.scene, this.camera);
+      this.animate();
     });
   },
   computed: {
@@ -141,8 +173,8 @@ export default {
 #container {
   min-width: 1366px;
   min-height: 768px;
-  width: 100%;
-  height: 100%;
+  // width: 100%;
+  // height: 100%;
   // display: none;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Date: 2020-09-29 22:47:11
+ * @LastEditors: 曾令宇
+ * @LastEditTime: 2020-09-29 22:47:38
+ * @FilePath: \gx-demo\vue.config.js
+ */
+module.exports = {
+    devServer: {
+        port: 8081
+    }
+}
